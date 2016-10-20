0001e42d762b017decbe5bbd0ca3d138501d102f
Work <asanokunimitsu+git@gmail.com>
2016-10-20 18:56:12 +0900

    First commit

---
+++ MapApp/Pin.swift
@@ -0,0 +1,57 @@
+//
+//  Pin.swift
+//  MapApp
+//
+//  Created by Work on 2016/10/20.
+//  Copyright © 2016年 sparta-asano.jp. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+// ピン情報の保持とUserDefaultsとの変換を行うクラス
+class Pin: NSObject, MKAnnotation {
+    
+    // 位置情報
+    var coordinate: CLLocationCoordinate2D = CLLocationCoordinate2D()
+    
+    // 追加時の入力テキスト
+    var title: String?
+    
+    // 位置情報とテキストを格納した状態のオブジェクトを返します
+    init(geo:CLLocationCoordinate2D, text: String?){
+        coordinate = geo
+        title = text
+    }
+    
+    // UserDefaultsから取り出した各値を変換したオブジェクトを返します
+    init(dictionary: [String: Any]) {
+        if let geo = dictionary["geo"] as? [String: CLLocationDegrees] {
+            if let latitude = geo["latitude"], let longitude = geo["longitude"] {
+                coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+            }
+        }
+        
+        if let tit = dictionary["title"] as? String {
+            title = tit
+        }
+    }
+    
+    // 保持中の値をUserDefaultsに登録できるように変換
+    func toDictionary() -> [String: Any] {
+        var dict: [String: Any] = [:]
+        
+        let geo: [String: CLLocationDegrees] = [
+            "latitude": coordinate.latitude,
+            "longitude": coordinate.longitude
+        ]
+        
+        dict["geo"] = geo
+        
+        if let tit = title {
+            dict["title"] = tit
+        }
+        
+        return dict
+    }
+}
--- MapApp/ViewController.swift
+++ MapApp/ViewController.swift
@@ -7,19 +7,87 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
+    // UserDefaultsの保存・読み込み時に使う名前
+    let userDefName = "pins"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // マップでロングタップイベントが反応するようにジェスチャーを登録します
+        let gesture = UILongPressGestureRecognizer(target: self, action: #selector(ViewController.longTapMapView(_:)));
+        mapView.addGestureRecognizer(gesture)
+        
+        // 保存されているピンを配置
+        loadPins()
     }
+    
+    // マップ上をロングタップした際にピンを登録
+    func longTapMapView(_ gesture: UILongPressGestureRecognizer) {
+        
+        // 位置情報を取得します。
+        let point = gesture.location(in: view)
+        let geo = mapView.convert(point, toCoordinateFrom: mapView)
+        
+        // アラートの作成
+        let alert = UIAlertController(title: "スポット登録", message: "この場所に残すメッセージを入力してください。", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "キャンセル", style: .cancel, handler: nil))
+        alert.addAction(UIAlertAction(title: "登録", style: .default, handler: { (action) -> Void in
+            // 登録ボタンのアクション
+            
+            let pin = Pin(geo: geo, text: alert.textFields?.first?.text)
+            self.mapView.addAnnotation(pin)
+            
+            self.savePin(pin)
+        }))
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        // ピンに登録するテキスト用の入力フィールドをアラートに追加します。
+        alert.addTextField(configurationHandler: { (textField) in
+            textField.placeholder = "メッセージ"
+        })
+        
+        // アラートの表示
+        present(alert, animated: true, completion: nil)
     }
-
-
+    
+    // 既に保存されているピンを取得
+    func loadPins() {
+        let userDefaults = UserDefaults.standard
+        
+        if let savedPins = userDefaults.object(forKey: userDefName) as? [[String: Any]] {
+                
+            // 現在のピンを削除
+            self.mapView.removeAnnotations(self.mapView.annotations)
+            
+            for pinInfo in savedPins {
+                let newPin = Pin(dictionary: pinInfo)
+                self.mapView.addAnnotation(newPin)
+            }
+        }
+    }
+    
+    // ピンの保存
+    func savePin(_ pin: Pin) {
+        let userDefaults = UserDefaults.standard
+        
+        // 保存するピンをUserDefaults用に変換します。
+        let pinInfo = (pin.toDictionary())
+        
+        if var savedPins = userDefaults.object(forKey: userDefName) as? [[String: Any]] {
+            // すでにピン保存データがある場合、それに追加する形で保存します。
+            savedPins.append(pinInfo)
+            userDefaults.set(savedPins, forKey: userDefName)
+            
+        } else {
+            // まだピン保存データがない場合、新しい配列として保存します。
+            let newSavedPins: [[String: Any]] = [pinInfo]
+            userDefaults.set(newSavedPins, forKey: userDefName)
+        }
+    }
 }
 
